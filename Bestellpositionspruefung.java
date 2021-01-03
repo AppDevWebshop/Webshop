b5dc1d4cc424ffd410ea765c6dae478a9a450706
DonErkens <76523942+DonErkens@users.noreply.github.com>
2021-01-03 14:54:07 +0100

    Update Bestellpositionspruefung.java

--- Bestellpositionspruefung.java
+++ Bestellpositionspruefung.java
@@ -1,25 +1,28 @@
 package com.company.Webshop;
 
-import com.company.Webshop.Artikel;
-import com.company.Webshop.Warenkorb;
-
-public class Bestellpositionspruefung{
+public class Bestellpositionspruefung extends Warenkorb{
 	
 	public static void main(String[] args) {
-			for(int i = 0; i <= 100; i++)
-			{	
-				if(Warenkorb.Artikelmenge[i] = 0)							 	//Abbruch Prüfung für leeren Warenkorb (muss noch mit Array angepasst werden)
-					System.out.println("Es wurde ein Fehler erkannt. Prüfen Sie den Warenkorb");	
-			}
 	}
-	public static void pruefungverschiedeneartikel(String[] args) {
+		public void leererwarenkorb(String[] args){
+		
+		for (int i = 0; i < inhaltWarenkorb.length; i++) 
+		{							
+			for (int j = 0; j < inhaltWarenkorb.length; j++) 
+				if(inhaltWarenkorb[i][j] == 0) {														//Abbruch Prüfung für leeren Warenkorb (muss noch mit Array angepasst werden)
+					System.out.println("Der Warenkorb enthält eine leere Artikelmenge. Prüfen Sie den Warenkorb!");	
+				}
+		}
+		}
+		
+		public void pruefungverschiedeneartikel(String[] args) {
 		
 		//Es dürfen nur 100 Artikel im Warenkorb sein, Menge ist dabei egal
 		
-		if(Warenkorb.Artikelposition[i] > 100)										//Artikelnummer kann schlecht geprüft werden, es muss eine laufende Nummer geben
-			System.out.println("Es wurden zu viele Artikel bestellt. Passen Sie den Warenkorb an");
+		if(inhaltWarenkorb.length > 100)																//Länge des Arrays wird geprüft
+			System.out.println("Es wurden zu viele Artikel bestellt. Passen Sie den Warenkorb an!");
 	}
-
+	
 }
 
 
