d715853b5d22f8e249209bf23129e00118ccc2e2
DonErkens <76523942+DonErkens@users.noreply.github.com>
2021-01-06 21:02:34 +0100

    Update Bestellpositionspruefung.java

--- Bestellpositionspruefung.java
+++ Bestellpositionspruefung.java
@@ -4,27 +4,18 @@ public class Bestellpositionspruefung extends Warenkorb{
 		
 		//Abbruch Prüfung für leeren Warenkorb 	
 			
-		public boolean leererwarenkorb(){   // prueft ob die Menge leer ist
-
+		public boolean leererwarenkorb(){   						//Prüfung ob ein Feld (Artikelnummer, Artikelmenge) leer ist
 			   for(int i = 0; i< inhaltWarenkorb.length;i++)
 			   	{
 				   for (int j = 0; j < inhaltWarenkorb.length;j++) 
-				   	{
+				   {
 					   if(inhaltWarenkorb[i][j] != 0){
 						   return false;
-					 }
+					  }
 				   }
-			 	}
-			   	return true;												
+				  
+			   	}
+			   	return true;	
+			}
 		}
-		
-		public void pruefungverschiedeneartikel() {
-		
-		//Es dürfen nur 100 Artikel im Warenkorb sein, Artikelmenge ist dabei egal
-		
-		if(inhaltWarenkorb.length > 100)																//Länge des Arrays wird geprüft
-			System.out.println("Es wurden zu viele Artikel bestellt. Passen Sie den Warenkorb an!");
-		}
-	
-}
  
