1770afa377aeafd0430d81f17cb89ba378d3f5da
alexprummern <76532996+alexprummern@users.noreply.github.com>
2021-01-11 20:52:41 +0100

    Update Warenkorb.java

--- Warenkorb.java
+++ Warenkorb.java
@@ -3,80 +3,64 @@ package com.company.Webshop;
 public class Warenkorb {
 
     //Attribute
-
-    private  int k = 0;
-    private int artikelmenge;
-    protected int[][] inhaltWarenkorb;
+    private int postenZaehler = 0;
+    //private int artikelmenge;
+    private int[][] inhaltWarenkorb;
 
 
     //Konstruktor
     public Warenkorb() {
-        inhaltWarenkorb = new int[5][2];
+        inhaltWarenkorb = new int[100][2];
 
     }
 
+
     //Methoden
-
     public int setInhaltWarenkorb(int artikelnummer, int menge) {
-
-        k= this.k;
+        //postenZaehler = this.postenZaehler;
         for (int i = 0; i < inhaltWarenkorb.length; i++) {
             for (int j = 0; j < inhaltWarenkorb[i].length; j++)
 
                 if (inhaltWarenkorb[i][j] == 0) {
                     inhaltWarenkorb[i][j] = artikelnummer;
-                    inhaltWarenkorb[i][j+1] = menge;
+                    inhaltWarenkorb[i][j + 1] = menge;
+                    break;
+                } else {
+                    postenZaehler++;
+                    inhaltWarenkorb[i + postenZaehler][j] = artikelnummer;
+                    inhaltWarenkorb[i + postenZaehler][j + 1] = menge;
                     break;
                 }
-
-                else  {
-                    k++;
-                    //System.out.println("Test");
-                    inhaltWarenkorb[i+k][j] = artikelnummer;
-                    inhaltWarenkorb[i+k][j+1] = menge;
-                    break;
-                }
-
             break;
-
         }
-
-        //printArray();
-        //System.out.println(k);
-        return k;
+        return postenZaehler;
     }
 
 
-    public void löscheArtikel(int artikelNummer) {
-
+    //Artikel aus Array löschen
+    public void loescheArtikel(int artikelNummer) {
 
         for (int i = 0; i < inhaltWarenkorb.length; i++) {
             for (int j = 0; j < inhaltWarenkorb[i].length; j++)
-
                 if (inhaltWarenkorb[i][j] == artikelNummer) {
                     inhaltWarenkorb[i][j] = 0;
                     inhaltWarenkorb[i][j + 1] = 0;
-                    //printArray();
                     break;
-
                 }
-
         }
     }
 
 
-    public void ändereAnzahl(int artikelNummer, int menge){
+    //Anzahl eines Artikels im Array ändern
+    public void aendereAnzahl(int artikelNummer, int menge) {
 
         for (int i = 0; i < inhaltWarenkorb.length; i++) {
             for (int j = 0; j < inhaltWarenkorb[i].length; j++)
 
                 if (inhaltWarenkorb[i][j] == artikelNummer) {
                     inhaltWarenkorb[i][j + 1] = menge;
-                    //printArray();
                     break;
-
                 }
-
         }
     }
 
@@ -84,20 +68,14 @@ public class Warenkorb {
 
         //Arrayinhalt anzeigen lassen
         public void printArray () {
-            for(int i=0; i<inhaltWarenkorb.length; i++)
-            {
-                for(int j=0; j<inhaltWarenkorb[i].length; j++)
-                    System.out.print(inhaltWarenkorb[i][j]+ " ");
+            for (int i = 0; i < inhaltWarenkorb.length; i++) {
+                for (int j = 0; j < inhaltWarenkorb[i].length; j++)
+                    System.out.print(inhaltWarenkorb[i][j] + " ");
                 System.out.println();
 
             }
         }
 
 
-        //setter
-        public void setArtikelmenge ( int artikelmenge){
-            this.artikelmenge = artikelmenge;
-        }
 
-    }
-
+}
