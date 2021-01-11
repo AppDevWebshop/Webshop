b35b5e903adfeaf99298b05803612769c303926f
alexprummern <76532996+alexprummern@users.noreply.github.com>
2021-01-11 20:52:07 +0100

    Update Bestellung.java

--- Bestellung.java
+++ Bestellung.java
@@ -1,67 +1,35 @@
 package com.company.Webshop;
+import com.company.Warenkorb;
 
-public class Bestellung extends Bestellpositionspruefung{
+public class Bestellung {
+
 
     //Attribute
-
     private double preisDerBestellung;
     private double versandKosten;
 
-    //Konstruktor
 
-   public Bestellung(){
+    //Konstruktor
+    public Bestellung() {
     }
 
 
-
     //Methoden
-
-    public void berechnungVersandkosten(){
-        if (preisDerBestellung <= 20 )
+    public void sendeBestellbestaetigung(int preisDerBestellung) {
+        if (preisDerBestellung <= 20)
             versandKosten = 5.95;
 
-        else if ( preisDerBestellung>20 & preisDerBestellung<=50 )
+        else if (preisDerBestellung > 20 & preisDerBestellung <= 50)
             versandKosten = 2.95;
 
         else if (preisDerBestellung > 50)
             versandKosten = 0;
 
         else System.out.println("Fehler");
-    }
 
-    public void sendeBestellbestätigung(){
-        System.out.println("Ihre Bestellung war erfolgreich. Ihre Rechnungssumme beträgt " + (versandKosten+preisDerBestellung) + "€.");
-
-    }
-    
-    public void berechnungPreis(){
-        for(int i=0;i<=warenkorb.length; i++)
-	{
-		for(int j=0; j<inhaltWarenkorb[i].length; j++)
-	        (preisDerBestellung = preisDerBestellung + inhaltWarenkorb[i]*inhalt.Warenkorb[j])
+        System.out.println("Ihre Bestellung war erfolgreich. Ihre Rechnungsumme beträgt " + (versandKosten + preisDerBestellung) + "€");
+        System.out.println("Ihre Lieferadresse lautet:");
 
     }
 
-    public int Artikel.getArtikelnummer;
-    //getter
-    
-    
-    //public double getVersandKosten() {
-    //    return versandKosten;
-    //}
-    
-     //public int getArtikelnummer() {
-     //   return artikelnummer;
-     //}
-    
-     public int getArtikelmenge() {
-        return artikelmenge;
-    }
-    //
-
-    //setter
-    public void setPreisDerBestellung(double preisDerBestellung) {
-        this.preisDerBestellung = preisDerBestellung;
-    }
-}   //
-
+}
